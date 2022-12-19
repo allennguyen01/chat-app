f7bf17bfdd28f031011ac35b20ad99049235f2c3
Allen Nguyen <anhn321@gmail.com>
2022-12-18 16:58:26 -0800

    A2.3 Use MVC and create objects to represent pages

--- client/app.js
+++ client/app.js
@@ -1,84 +1,102 @@
 function main() {
+	const lobbyView = new LobbyView();
+	const chatView = new ChatView();
+	const profileView = new ProfileView();
+
 	function renderRoute() {
 		let url = window.location.hash;
 		const pageView = document.getElementById("page-view");
 
-		switch (url) {
-			case "#/":
+		switch (true) {
+			case url === "#/":
 				emptyDOM(pageView);
-				const pageContent = createDOM(
-					`<div class="content">
-            <ul class="room-list">
-              <li><a href="#/chat/room-1">Room 1</a></li>
-              <li><a href="#/chat/room-2">Room 2</a></li>
-              <li><a href="#/chat/room-3">Room 3</a></li>
-            </ul>
-            <div class="page-control">
-              <input type="text" name="" id="create-room" placeholder="Room Name" />
-              <button>Create Room</button>
-            </div>
-          </div>`
-				);
-				pageView.appendChild(pageContent);
+				pageView.appendChild(lobbyView.elem);
 				break;
-			case "#/profile":
+			case url === "#/profile":
 				emptyDOM(pageView);
-				const profileContent = createDOM(
-					`<div class="content">
-            <div class="profile-form">
-              <div class="form-field">
-                <label for="username">Username</label>
-                <input type="text" name="username" id="username" />
-              </div>
-              <div class="form-field">
-                <label for="password">Password</label>
-                <input type="password" name="password" id="password" />
-              </div>
-              <div class="form-field">
-                <label for="profile-pic">Profile picture</label>
-                <input type="file" name="profile_pic" id="profile-pic" />
-              </div>
-            </div>
-            <div class="page-control">
-              <button>Create account</button>
-            </div>
-          </div>`
-				);
-				pageView.appendChild(profileContent);
+				pageView.appendChild(profileView.elem);
 				break;
-		}
-		if (url.startsWith("#/chat")) {
-			emptyDOM(pageView);
-			const chatContent = createDOM(
-				`<div class="content">
-            <h4 class="room-name">Italy</h4>
-            <div class="message-list">
-              <div class="message">
-                <span class="message-user">Mario:</span>
-                <span class="message-text">It's a me</span>
-              </div>
-              <div class="message my-message">
-                <span class="message-user">Luigi:</span>
-                <span class="message-text">Mama mia!</span>
-              </div>
-            </div>
-            <div class="page-control">
-              <textarea name="" id=""></textarea>
-              <button>Send</button>
-            </div>
-          </div>`
-			);
-			pageView.appendChild(chatContent);
+			case url.startsWith("#/chat"):
+				emptyDOM(pageView);
+				pageView.appendChild(chatView.elem);
 		}
 	}
 
 	renderRoute();
 	window.addEventListener("popstate", renderRoute);
 
-	cpen322.export(arguments.callee, { renderRoute });
+	cpen322.export(arguments.callee, { renderRoute, lobbyView, chatView, profileView });
 }
 
 window.addEventListener("load", main);
+
+class LobbyView {
+	constructor() {
+		this.elem = createDOM(
+			`<div class="content">
+        <ul class="room-list">
+          <li><a href="#/chat/room-1">Room 1</a></li>
+          <li><a href="#/chat/room-2">Room 2</a></li>
+          <li><a href="#/chat/room-3">Room 3</a></li>
+        </ul>
+        <div class="page-control">
+          <input type="text" name="" id="create-room" placeholder="Room Name" />
+          <button>Create Room</button>
+        </div>
+      </div>`
+		);
+	}
+}
+
+class ChatView {
+	constructor() {
+		this.elem = createDOM(
+			`<div class="content">
+          <h4 class="room-name">Italy</h4>
+          <div class="message-list">
+            <div class="message">
+              <span class="message-user">Mario:</span>
+              <span class="message-text">It's a me</span>
+            </div>
+            <div class="message my-message">
+              <span class="message-user">Luigi:</span>
+              <span class="message-text">Mama mia!</span>
+            </div>
+          </div>
+          <div class="page-control">
+            <textarea name="" id=""></textarea>
+            <button>Send</button>
+          </div>
+        </div>`
+		);
+	}
+}
+
+class ProfileView {
+	constructor() {
+		this.elem = createDOM(
+			`<div class="content">
+        <div class="profile-form">
+          <div class="form-field">
+            <label for="username">Username</label>
+            <input type="text" name="username" id="username" />
+          </div>
+          <div class="form-field">
+            <label for="password">Password</label>
+            <input type="password" name="password" id="password" />
+          </div>
+          <div class="form-field">
+            <label for="profile-pic">Profile picture</label>
+            <input type="file" name="profile_pic" id="profile-pic" />
+          </div>
+        </div>
+        <div class="page-control">
+          <button>Create account</button>
+        </div>
+      </div>`
+		);
+	}
+}
 
 // Removes the contents of the given DOM element (equivalent to elem.innerHTML = '' but faster)
 function emptyDOM(elem) {
