a5eec007b6cefbdb49a85db573f495e45a9728b3
Allen Nguyen <anhn321@gmail.com>
2022-12-20 00:55:50 -0800

    A2.5 Use OOP to represent app state

--- client/app.js
+++ client/app.js
@@ -1,3 +1,6 @@
+cpen322.setDefault("image", "assets/everyone-icon.png");
+cpen322.setDefault("testRoomId", "room-1");
+
 function main() {
 	const lobbyView = new LobbyView();
 	const chatView = new ChatView();
@@ -29,6 +32,42 @@ function main() {
 }
 
 window.addEventListener("load", main);
+
+class Room {
+	constructor(id, name, image = "assets/everyone-icon.png", messages = []) {
+		this.id = id;
+		this.name = name;
+		this.image = image;
+		this.messages = messages;
+	}
+
+	addMessage(username, text) {
+		if (text.trim() == "") return;
+		const message = { username: username, text: text };
+		this.messages.push(message);
+	}
+}
+
+class Lobby {
+	constructor() {
+		this.rooms = {
+			"room-1": new Room("room-1", "One"),
+			"room-2": new Room("room-2", "Two"),
+			"room-3": new Room("room-3", "Three"),
+			"room-4": new Room("room-4", "Four"),
+		};
+	}
+
+	getRoom(roomId) {
+		for (const id in this.rooms) {
+			if (roomId === id) return this.rooms[id];
+		}
+	}
+
+	addRoom(id, name, image, messages) {
+		this.rooms[id] = new Room(id, name, image, messages);
+	}
+}
 
 class LobbyView {
 	constructor() {
